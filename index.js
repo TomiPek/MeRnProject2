2fabe8a7410240ad9a7da7f9124cdd50f0c83e40
TomiPek <72078799+TomiPek@users.noreply.github.com>
2021-03-21 12:29:56 +0200

    Päivityksiä

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000; // Portin sijainti
 
-let express = require("express");
+let express = require("express"); // Express käyttöön
 let app = express();
-app.use(express.static("./public/")); // appille tiedostojen sijainti
+app.use(express.static("./public/")); // apille tiedostojen sijainti. Esim tyylitiedosto.
 
-app.set("view engine", "ejs");
+app.set("view engine", "ejs"); // ejs templaten käyttöönotto
 
-app.locals.pretty = true;
+tekija = "Made by: Tomi Pekurinen"; // footteria varten tekijä muuttuja
 
 app.get("/", (req, res) => {
-  res.render("pages/index");
+  // funktio pääsivua varten
+  res.render("pages/index", {
+    // Ohjetekstin muuttuja, sijainti on partials/pubit.ejs
+    hello:
+      "Welcome! Here you can find information about breweries around United States",
+  });
 });
 
 app.get("/cities", (req, res) => {
+  // kaupungit välilehti
   res.render("pages/towns");
 });
 app.get("/upcoming", (req, res) => {
+  // tulevat
   res.render("pages/coming");
 });
 app.listen(PORT, () => {
+  // kuunnellaan haluttua porttia
   console.log("Kuunnellaan porttia 5000");
 });
